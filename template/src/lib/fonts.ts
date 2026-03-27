7bfcfcd57ef214ffeea0d6ac5e71c3d4b81e62c8
Anuradha Mudalige <anuradhais@live.com>
2026-03-27 15:30:29 +0800

    Removed custom content

--- template/src/lib/fonts.ts
+++ template/src/lib/fonts.ts
@@ -4,42 +4,42 @@ export const CustomFont = localFont({
   variable: '--custom-font-family',
   src: [
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRg.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRgIt.otf',
       weight: '300',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRg.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRgIt.otf',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRg.otf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRgIt.otf',
       weight: '600',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRg.otf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/MondiraScript_PERSONAL_USE_ONLY.otf',
+      path: '../../public/fonts/CoolveticaRgIt.otf',
       weight: '700',
       style: 'italic',
     },
